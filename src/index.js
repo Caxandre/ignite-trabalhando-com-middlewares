3eb95e7f2cc5efa0e3b20a80053d1332d51170d6
Carlos Perrout <carlosperrout@gmail.com>
2021-04-03 00:20:26 -0300

    ending challenge

--- src/index.js
+++ src/index.js
@@ -11,18 +11,81 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+  
+  const user = users.find((user) => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "User account does not exists!"
+    });
+  }
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  if(user.pro === false && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: "User account does not exists!"
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+
+  const {id} = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "User account does not exists!"
+    });
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({
+      error: "Id must be a valid UUID format!"
+    });
+  };
+
+  const todo = user.todos.find(
+    (td) => td.id === id 
+  );
+  
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const {id} = request.params;
+  
+  const user = users.find((user) => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "User account does not exists!"
+    });
+  }
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
